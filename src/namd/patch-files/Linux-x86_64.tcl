60b9f798a975519ef25748f3d9a1291e76b35b5a
Jim Hayes <jhayes@sdsc.edu>
2012-07-19 22:58:21 +0000

    Replace hard-coded tcl version w/calculated one.

--- src/namd/patch-files/Linux-x86_64.tcl
+++ src/namd/patch-files/Linux-x86_64.tcl
@@ -1,5 +1,5 @@
 TCLDIR=ROOTDIR/linux-x86_64
 TCLINCL=-I$(TCLDIR)/include
-TCLLIB=-L$(TCLDIR)/lib -ltcl8.4 -ldl
+TCLLIB=-L$(TCLDIR)/lib -ltcl`echo 'puts $$tcl_version' | tclsh` -ldl
 TCLFLAGS=-DNAMD_TCL
 TCL=$(TCLINCL) $(TCLFLAGS)
