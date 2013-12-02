dbb05f93959655fc651dddca2c02641bf8ce0fb0
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 13:51:14 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/apbs/version.mk
+++ src/apbs/version.mk
@@ -1,4 +1,16 @@
-NAME	= apbs_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION	= 1.3
-RELEASE	= 4
-RPM.EXTRAS = AutoReq:No
+NAME               = apbs_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 1.3
+RELEASE            = 4
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/apbs
+
+SRC_SUBDIR         = apbs
+
+SOURCE_NAME        = apbs
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG)
+
