01304f2f269c4d2ec7405057a20978f299a32f5d
Trevor Cooper <tcooper@sdsc.edu>
2013-12-02 14:18:40 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/gromacs/version.mk
+++ src/gromacs/version.mk
@@ -1,4 +1,20 @@
-NAME    = gromacs_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 4.6.2
-RELEASE = 4
-RPM.EXTRAS = AutoReq:No
+NAME               = gromacs_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 4.6.2
+RELEASE            = 4
+RPM.EXTRAS         = AutoReq:No
+PKGROOT            = /opt/gromacs
+
+SRC_SUBDIR         = gromacs
+
+SOURCE_NAME        = gromacs
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+GMXTEST_NAME       = gmxtest
+GMXTEST_PKG        = $(GMXTEST_NAME).$(SOURCE_SUFFIX)
+GMXTEST_DIR        = $(GMXTEST_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG) $(GMXTEST_PKG)
+
