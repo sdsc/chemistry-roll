56c5607e0d7ca694be0cea096ba8166a9bf1ac68
Jerry Greenberg <jpg@gcn-20-88.sdsc.edu>
2014-10-19 11:36:43 -0700

    accidently checked in temp file; removed them

--- BUILD/chemistry-roll-test-1.0/version.mk
+++
@@ -1,6 +0,0 @@
-NAME       = chemistry-roll-test
-VERSION    = 1.0
-RELEASE    = 10
-PKGROOT    = /root/rolltests
-
-RPM.EXTRAS = AutoReq:No
--- BUILD/roll-chemistry-6.1/version.mk
+++
@@ -1,6 +0,0 @@
-ROLLNAME = chemistry
-RELEASE  = 10
-COLOR    = orange
-RPM.EXTRAS = AutoReq:No
-
-REDHAT.ROOT = $(PWD)
