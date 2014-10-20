77e1c49f3d595e53c43830fb431cd32702f63b9e
Jim Hayes <jhayes@sdsc.edu>
2014-10-20 05:41:21 -0700

    Change default MPI.

--- src/namd/version.mk
+++ src/namd/version.mk
@@ -7,7 +7,7 @@ endif
 COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
 
 ifndef ROLLMPI
-  ROLLMPI = openmpi
+  ROLLMPI = rocks-openmpi
 endif
 MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
