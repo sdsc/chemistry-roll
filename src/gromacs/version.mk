82379f29d74e04dbb634b01b2e4da6cd322c6330
Jim Hayes <jhayes@sdsc.edu>
2015-01-16 00:16:14 -0800

    Have gromacs retrieve fftw source from stash instead of web.

--- src/gromacs/version.mk
+++ src/gromacs/version.mk
@@ -10,10 +10,17 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-gromacs
 VERSION        = 5.0.4
-RELEASE        = 0
+RELEASE        = 1
 PKGROOT        = /opt/gromacs
 
 SRC_SUBDIR     = gromacs
+
+FFTW_NAME      = fftw
+FFTW_SUFFIX    = tar.gz
+FFTW_VERSION   = 3.3.4
+FFTW_PKG       = $(FFTW_NAME)-$(FFTW_VERSION).$(FFTW_SUFFIX)
+FFTW_DIR       = $(FFTW_PKG:%.$(FFTW_SUFFIX)=%)
+FFTW_MD5       = 2edab8c06b24feeb3b82bbb3ebf3e7b3
 
 SOURCE_NAME    = gromacs
 SOURCE_SUFFIX  = tar.gz
@@ -26,6 +33,6 @@ GMXTEST_SUFFIX = tar.gz
 GMXTEST_PKG    = $(GMXTEST_NAME).$(GMXTEST_SUFFIX)
 GMXTEST_DIR    = $(GMXTEST_PKG:%.$(GMXTEST_SUFFIX)=%)
 
-TAR_GZ_PKGS    = $(SOURCE_PKG) $(GMXTEST_PKG)
+TAR_GZ_PKGS    = $(SOURCE_PKG) $(FFTW_PKG) $(GMXTEST_PKG)
 
 RPM.EXTRAS     = AutoReq:No
