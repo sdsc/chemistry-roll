57f7eee403d79e1243219919aea91b146902cdc4
Jim Hayes <jhayes@sdsc.edu>
2015-09-15 11:34:58 -0700

    Set ROLLMPI based on gromacs_othermpi ROLLOPTS value when present. Resolves APS_195.

--- src/gromacs-modules/version.mk
+++ src/gromacs-modules/version.mk
@@ -1,8 +1,18 @@
+ifndef ROLLCOMPILER
+  ROLLCOMPILER = gnu
+endif
+
+ifneq ("$(ROLLOPTS)", "$(subst gromacs_othermpi=,,$(ROLLOPTS))")
+  override ROLLMPI = $(subst gromacs_othermpi=,,$(filter gromacs_othermpi=%,$(ROLLOPTS)))
+else ifndef ROLLMPI
+  ROLLMPI = rocks-openmpi
+endif
+
 PACKAGE     = gromacs
 CATEGORY    = applications
 
 NAME        = sdsc-$(PACKAGE)-modules
-RELEASE     = 4
+RELEASE     = 5
 PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
