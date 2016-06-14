3c7ce8baa6bab2b6d898c5ab2e78667d70c58bdd
Jerry Greenberg <jpg@comet-01-65.local>
2016-06-14 11:09:24 -0700

    APS-354: as is traditional, I forgot to increase the release numbers for cp2k and lammps in the prior commit

--- src/lammps/version.mk
+++ src/lammps/version.mk
@@ -10,7 +10,7 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-lammps
 VERSION        = $(shell date -d "$(SOURCE_VERSION)" +%Y%m%d)
-RELEASE        = 1
+RELEASE        = 2
 PKGROOT        = /opt/lammps
 
 SRC_SUBDIR     = lammps
