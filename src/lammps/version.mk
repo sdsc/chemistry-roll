042c677c165b29dcd8ec9c02aebe18062666cf30
Jerry Greenberg <jpg@sdsc.edu>
2017-07-05 11:55:50 -0700

    APS-440: more additions for lammps 3/31/17

--- src/lammps/version.mk
+++ src/lammps/version.mk
@@ -8,16 +8,21 @@ ifndef ROLLMPI
 endif
 MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
+CUDAVERSION=cuda
+ifneq ("$(ROLLOPTS)", "$(subst lammps_othercuda=,,$(ROLLOPTS))")
+  CUDAVERSION = $(subst lammps_othercuda=,,$(filter lammps_othercuda=%,$(ROLLOPTS)))
+endif
+
 NAME           = sdsc-lammps
 VERSION        = $(shell date -d "$(SOURCE_VERSION)" +%Y%m%d)
-RELEASE        = 3
+RELEASE        = 0
 PKGROOT        = /opt/lammps
 
 SRC_SUBDIR     = lammps
 
 SOURCE_NAME    = lammps
 SOURCE_SUFFIX  = tar.gz
-SOURCE_VERSION := 16Feb16
+SOURCE_VERSION := 31Mar17
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
