2f3b19ad114d5dd4ab97f702115cee170bf58251
Jerry Greenberg <jpg@comet-01-67.local>
2017-07-17 19:08:51 -0700

    APS-440: removal of no longer existing lammps "styles" and addition of some new ones

--- src/lammps/version.mk
+++ src/lammps/version.mk
@@ -8,16 +8,31 @@ ifndef ROLLMPI
 endif
 MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
+CUDAVERSION=cuda
+ifneq ("$(ROLLOPTS)", "$(subst cuda=,,$(ROLLOPTS))")
+  CUDAVERSION = $(subst cuda=,,$(filter cuda=%,$(ROLLOPTS)))
+endif
+
+ifneq ("$(ROLLOPTS)", "$(subst lammps_othercuda=,,$(ROLLOPTS))")
+  CUDAVERSION = $(subst lammps_othercuda=,,$(filter lammps_othercuda=%,$(ROLLOPTS)))
+endif
+
+L_MKL_VERSION=mkl
+ifneq ("$(ROLLOPTS)", "$(subst lammps_othermkl,,$(ROLLOPTS))")
+    L_MKL_VERSION = $(subst lammps_othermkl=,,$(filter lammps_othermkl=%,$(ROLLOPTS)))
+endif
+
+
 NAME           = sdsc-lammps
 VERSION        = $(shell date -d "$(SOURCE_VERSION)" +%Y%m%d)
-RELEASE        = 3
+RELEASE        = 0
 PKGROOT        = /opt/lammps
 
 SRC_SUBDIR     = lammps
 
 SOURCE_NAME    = lammps
 SOURCE_SUFFIX  = tar.gz
-SOURCE_VERSION := 16Feb16
+SOURCE_VERSION := 31Mar17
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
