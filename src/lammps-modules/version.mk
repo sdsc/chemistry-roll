825d699ed11edafec56d475b1ee110f3df99f93c
Jerry Greenberg <jpg@comet-01-67.local>
2017-07-20 14:03:52 -0700

    APS-440: add option to choose different mpi for lammps

--- src/lammps-modules/version.mk
+++ src/lammps-modules/version.mk
@@ -3,6 +3,13 @@ ifneq ("$(ROLLOPTS)", "$(subst cuda,,$(ROLLOPTS))")
   ROLLCUDA = cuda
 endif
 
+ifneq ("$(ROLLOPTS)", "$(subst lammps_othermpi=,,$(ROLLOPTS))")
+  override ROLLMPI = $(subst lammps_othermpi=,,$(filter lammps_othermpi=%,$(ROLLOPTS)))
+else ifndef ROLLMPI
+  ROLLMPI = rocks-openmpi
+endif
+MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
+
 PACKAGE     = lammps
 CATEGORY    = applications
 
