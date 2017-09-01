765b0d602ef6cf3c175691d99d62d6f878727c6d
Jerry Greenberg <jpg@sdsc.edu>
2017-08-31 18:46:47 -0700

    change author add namd 2.12 to namd module package version.mk

--- src/namd-modules/version.mk
+++ src/namd-modules/version.mk
@@ -2,13 +2,13 @@ PACKAGE     = namd
 CATEGORY    = applications
 
 NAME        = sdsc-$(PACKAGE)-modules
-RELEASE     = 7
+RELEASE     = 8
 PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
 VERSION_INC = version.inc
 include $(VERSION_INC)
 
-EXTRA_MODULE_VERSIONS = 2.9 2.11
+EXTRA_MODULE_VERSIONS = 2.9 2.11 2.12
 
 RPM.EXTRAS  = AutoReq:No
