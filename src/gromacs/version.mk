f3c70d5867f0d3fb3772f1892d9d241107814295
Jerry Greenberg <jpg@sdsc.edu>
2019-02-26 16:16:26 -0800

    APS-995 added increment for release number

--- src/gromacs/version.mk
+++ src/gromacs/version.mk
@@ -24,7 +24,7 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-gromacs
 VERSION        = 2018.3
-RELEASE        = 2
+RELEASE        = 4
 PKGROOT        = /opt/gromacs
 
 SRC_SUBDIR     = gromacs
