2b9c0505bf0b6a3181102a891c81d2cedcce4daa
Jerry Greenberg <jpg@sdsc.edu>
2021-05-28 17:40:29 -0700

    APS-1129 apbs 3.0.0

--- src/apbs/version.mk
+++ src/apbs/version.mk
@@ -9,14 +9,14 @@ endif
 MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME                 = sdsc-apbs
-VERSION              = 1.5
-RELEASE              = 3
+VERSION              = 3.0.0
+RELEASE              = 0
 PKGROOT              = /opt/apbs
 
 SRC_SUBDIR           = apbs
 
 SOURCE_NAME          = apbs
-SOURCE_SUFFIX        = zip
+SOURCE_SUFFIX        = tar.gz
 SOURCE_VERSION       = $(VERSION)
 SOURCE_PKG           = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR           = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
@@ -45,8 +45,7 @@ MC_SOURCE_VERSION    = 1.5
 MC_SOURCE_PKG        = $(MC_SOURCE_NAME)-$(MC_SOURCE_VERSION).$(MC_SOURCE_SUFFIX)
 MC_SOURCE_DIR        = mc
 
-ZIP_PKGS             = $(SOURCE_PKG)
-TAR_GZ_PKGS          = $(MALOC_SOURCE_PKG) $(PUNC_SOURCE_PKG) $(GAMER_SOURCE_PKG) $(MC_SOURCE_PKG)
+TAR_GZ_PKGS          = $(MALOC_SOURCE_PKG) $(PUNC_SOURCE_PKG) $(GAMER_SOURCE_PKG) $(MC_SOURCE_PKG) $(SOURCE_PKG)
 
 RPM.PREFIX           = $(PKGROOT)
-RPM.EXTRAS           = AutoReq:No\nAutoProv:No
+RPM.EXTRAS           = AutoReq:No\nAutoProv:No\n%global _python_bytecompile_errors_terminate_build 0
